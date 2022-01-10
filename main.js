3a2091fab9abb42bec9ee6b0eba003724374a09e
major davis <majordav@msn.com>
2022-01-09 17:45:20 -0700

    added span tags to each letter in row-bottom, added functions to circles in js

--- main.js
+++ main.js
@@ -19,6 +19,43 @@ function switchColor1() {
 
     $('.circle-1').css('background-color', colors[currentColor++]);
 
-    setTimeout(switchColor1, 1000);
+    setTimeout(switchColor1, 2000);
 }
 switchColor1();
+
+
+var colors = ["#35deff", "#694bff", "#f97aff", "#f1cfb1"];
+
+var currentColor = 0;
+function switchColor2() {    
+    if (currentColor >= colors.length) currentColor = 0;
+
+    $('.circle-2').css('background-color', colors[currentColor++]);
+
+    setTimeout(switchColor2, 6000);
+}
+switchColor2();
+
+var colors = ["#35deff", "#694bff", "#f97aff", "#f1cfb1"];
+
+var currentColor = 0;
+function switchColor3() {    
+    if (currentColor >= colors.length) currentColor = 0;
+
+    $('.circle-3').css('background-color', colors[currentColor++]);
+
+    setTimeout(switchColor3, 4000);
+}
+switchColor3();
+
+var colors = ["#35deff", "#694bff", "#f97aff", "#f1cfb1"];
+
+var currentColor = 0;
+function switchColor4() {    
+    if (currentColor >= colors.length) currentColor = 0;
+
+    $('.circle-4').css('background-color', colors[currentColor++]);
+
+    setTimeout(switchColor4, 2000);
+}
+switchColor4();
